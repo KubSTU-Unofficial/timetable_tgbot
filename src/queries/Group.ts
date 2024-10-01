dbc52a3f169b411c150bea0bf63531081c8e3593
Electroplayer <vitaly.hawanski@yandex.ru>
2024-10-01 18:16:29 +0300

    При перенастройке, группы ИТК были сокращены

--- src/queries/Group.ts
+++ src/queries/Group.ts
@@ -1,65 +1,88 @@
-import { CallbackQuery } from "node-telegram-bot-api";
-import Query from "../structures/Query.js";
-import User from "../structures/User.js";
-import Cache from "../lib/Cache.js";
-import ScheduleModel from "../shared/models/ScheduleModel.js";
+import { CallbackQuery } from 'node-telegram-bot-api';
+import Query from '../structures/Query.js';
+import User from '../structures/User.js';
+import Cache from '../lib/Cache.js';
+import ScheduleModel from '../shared/models/ScheduleModel.js';
 
 interface KeyboardButton {
-    text: string,
-    callback_data: string
+    text: string;
+    callback_data: string;
 }
 
+let groupsInfo: { [key: number]: { substring: number; identifier: string } | undefined } = {
+    541: {
+        substring: 5,
+        identifier: 'ИТ',
+    },
+};
+
 export default class GroupQuery extends Query {
-    name = ["settings_kurs"];
-    sceneName = "settings";
+    name = ['settings_kurs'];
+    sceneName = 'settings';
 
     async exec(user: User, query: CallbackQuery): Promise<void> {
-        if(!query?.message?.text) return; // не знаю как, но на всякий случай
+        if (!query?.message?.text) return; // не знаю как, но на всякий случай
 
-        let db = user.dataBuffer.find(db => db.id == query.message?.message_id);
+        let db = user.dataBuffer.find((db) => db.id == query.message?.message_id);
 
-        if(!db) {
-            Cache.bot.sendMessage(query.message!.chat.id, "Похоже эта кнопка себя исчерпала");
-            return;
-        }
-    
-        db.kurs = +query.data!.slice(14,query.data!.length);
-    
-        let text: string  = query.message!.text;
-        let now: Date     = new Date();
-        let groupDate     = (now.getFullYear() - db.kurs + 1 - ( now.getMonth() >= 6 ? 0 : 1)).toString().substring(2);
-        let schedules     = await ScheduleModel.find({inst_id: db.inst_id!, group: { $regex: `^${groupDate}-`}}).exec();
-        let groups        = schedules.map(s => s.group);
-        
-        if(!groups || groups!.length == 0) {
-            Cache.bot.editMessageText(text.split("\n\n").slice(0,text.split("\n\n").length-1).join("\n\n") + "\n\nЧто-то пошло не так! Повтори попытку позже... \nЕсли проблема не уходит, обратись в поддержку: @Elektroplayer", {
-                chat_id: query.message.chat.id,
-                message_id: query.message.message_id,
-            });
-
+        if (!db) {
+            Cache.bot.sendMessage(query.message!.chat.id, 'Похоже эта кнопка себя исчерпала');
             return;
         }
 
-        let keyboard: KeyboardButton[][]  = [];
-        let buffer: KeyboardButton[]      = [];
+        db.kurs = +query.data!.slice(14, query.data!.length);
 
-        for(let i = 0;i<groups.length;i++) {
-            if(i%4==0 && i!=0) {
+        let text: string = query.message!.text;
+        let now: Date = new Date();
+        let groupDate = (now.getFullYear() - db.kurs + 1 - (now.getMonth() >= 6 ? 0 : 1)).toString().substring(2);
+        let schedules = await ScheduleModel.find({ inst_id: db.inst_id!, group: { $regex: `^${groupDate}-` } }).exec();
+        let groups = schedules.map((s) => s.group);
+        let groupInfo = groupsInfo[db.inst_id!];
+
+        if (!groups || groups!.length == 0) {
+            Cache.bot.editMessageText(
+                text
+                    .split('\n\n')
+                    .slice(0, text.split('\n\n').length - 1)
+                    .join('\n\n') +
+                    '\n\nЧто-то пошло не так! Повтори попытку позже... \nЕсли проблема не уходит, обратись в поддержку: @Elektroplayer',
+                {
+                    chat_id: query.message.chat.id,
+                    message_id: query.message.message_id,
+                },
+            );
+
+            return;
+        }
+
+        let keyboard: KeyboardButton[][] = [];
+        let buffer: KeyboardButton[] = [];
+
+        for (let i = 0; i < groups.length; i++) {
+            if (i % 4 == 0 && i != 0) {
                 keyboard.push(buffer);
                 buffer = [];
             }
             buffer.push({
-                text: groups[i].substring(3),
-                callback_data: "settings_group_"+groups[i]
+                text: groups[i].substring(groupInfo?.substring ?? 3),
+                callback_data: 'settings_group_' + groups[i],
             });
         }
 
         keyboard.push(buffer);
 
-        Cache.bot.editMessageText(text.split("\n\n").slice(0,text.split("\n\n").length-1).join("\n\n") + `\n\nВыбери свою группу. ${groupDate}-...`, {
-            chat_id: query.message.chat.id,
-            message_id: query.message.message_id,
-            reply_markup: { inline_keyboard: keyboard }
-        });
+        Cache.bot.editMessageText(
+            text
+                .split('\n\n')
+                .slice(0, text.split('\n\n').length - 1)
+                .join('\n\n') +
+                `\n\nВыбери свою группу: ${groupDate}-${groupInfo?.identifier ?? ''}...\n<i>Если группы видно не полностью, попробуй перевернуть телефон</i>`,
+            {
+                chat_id: query.message.chat.id,
+                message_id: query.message.message_id,
+                reply_markup: { inline_keyboard: keyboard },
+                parse_mode: 'HTML',
+            },
+        );
     }
 }
