2357cfd8044c2b2ce18de7a40d0567565ece4842
Electroplayer <vitaly.hawanski@yandex.ru>
2025-05-12 14:23:13 +0300

    Исправление ошибки с периодом в расписании преподавателей

--- src/structures/Teacher.ts
+++ src/structures/Teacher.ts
@@ -40,11 +40,14 @@ export default class Teacher extends BaseTeacher {
             (acc, lesson) => {
                 let out = `${lesson.number}. ${lesson.name} [${dict[lesson.type]}]\n` +
                     `  Аудитория: ${lesson.classroom}\n` +
-                    `  Группа: ${lesson.group}\n\n`;
+                    `  Группа: ${lesson.group}\n`;
 
-                if('nedType' in lesson.day && lesson.day.weeks.startDate && lesson.day.weeks.startDate > new Date()) out = `<i>${out}\nПериод: c ${lesson.day.weeks.from} по ${lesson.day.weeks.to} неделю</i>>`;
+                if(
+                    'nedType' in lesson.day && lesson.day.weeks.startDate &&
+                    !(lesson.day.weeks.startDate <= date && date.valueOf() < lesson.day.weeks.startDate.valueOf() + 1000 * 60 * 60 * 24 * 7 * (lesson.day.weeks.to - lesson.day.weeks.from))
+                ) out = `<i>${out}  Период: c ${lesson.day.weeks.from} по ${lesson.day.weeks.to} неделю</i>\n`;
 
-                return acc + out;
+                return acc + out + '\n';
             },
             '',
         );
@@ -54,7 +57,7 @@ export default class Teacher extends BaseTeacher {
         let schedule = await this.getFullRawSchedule();
 
         if(!schedule) return;
-        if(!schedule.length) return [`Здесь ничего нет...`];
+        if(!schedule.length) return [`Здесь ничего нет... <i>Возможно ты ошибся с именем преподавателя</i>`];
 
         let now = new Date();
         let curMonday = getMonday(now);
