da790de63ae00fc791de4bef4daa8a7af5136ca0
Electroplayer <vitaly.hawanski@yandex.ru>
2026-01-15 19:48:02 +0300

    Добавлен забытый "!"

--- src/structures/Query.ts
+++ src/structures/Query.ts
@@ -7,7 +7,7 @@ export default abstract class Query {
     errorCatcher(err: any) {
         let ignoreErrors = ["Error: ETELEGRAM: 400 Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message"];
 
-        if (ignoreErrors.includes(err.toString())) {
+        if (!ignoreErrors.includes(err.toString())) {
             console.log(`"${err}"`);
             console.log(err);
         }
