38a1be11410ccef94533b9081257ccf4e0d38e8a
Electroplayer <vitaly.hawanski@yandex.ru>
2026-01-30 20:54:52 +0300

    Добавлено исключение некоторых ошибок

--- src/events/PollingErrors.ts
+++ src/events/PollingErrors.ts
@@ -4,7 +4,14 @@ export default class PollingErrorsEvent extends Event {
     name = 'polling_error' as BotEvents;
 
     exec(err: Error): void {
-        console.log(`${err}`);
-        console.log(err);
+        let excludeErrors = [
+            "Error: ETELEGRAM: 502 Bad Gateway"
+        ]
+
+        if (excludeErrors.includes(`${err}`)) return console.log(`${err}`)
+        else {
+            console.log(`"${err}"`);
+            console.log(err);
+        }
     }
 }
